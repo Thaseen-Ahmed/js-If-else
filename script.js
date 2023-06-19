c27bc105fb7918a1c4e707d7c3e28cf166ad1dc1
Thaseen-Ahmed <125480253+Thaseen-Ahmed@users.noreply.github.com>
2023-06-19 20:01:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,8 +15,9 @@ function ageValid(){
         alert("You Can Think to drive")
     }
     if(a.value>18 & a.value<=80){
+         alert("You Are Eligiable to Drive");
         innerHTML=val.innerHTML="<div {backgroundcolour: green; height: 10vh; width:30%} ><h2>You are Eligible to apply for Driving License </div>"';
-            alert("You Are Eligiable to Drive");
+           
     }
     else{
         alert('Enter the age Between 18 and 70')
